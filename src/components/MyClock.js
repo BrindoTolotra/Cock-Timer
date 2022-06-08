ef008d9c774a4c82a1bb60bd8390a3eacf45ba98
Brindo Ratiharimanana <98638794+BrindoTolotra@users.noreply.github.com>
2022-06-08 22:01:35 +0300

    Update MyClock.js

--- src/components/MyClock.js
+++ src/components/MyClock.js
@@ -1,11 +1,9 @@
-
 import { useEffect, useState } from "react";
-
 
 export function MyClock(){
     const [hour,setHour] = useState(new Date().toLocaleTimeString());
 
-    // function qui fait changer l'heure chaque interval de 1000ms
+    
     const interval = () =>{
         window.setInterval(()=>{
             setHour(new Date().toLocaleTimeString());
