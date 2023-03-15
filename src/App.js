25ae596731d8151b51f3973aac62a48f2582cd0f
Brindo Ratiharimanana <98638794+BrindoTolotra@users.noreply.github.com>
2023-03-15 18:03:21 +0300

    refactor: removed comment

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import { Mytimer } from "./components/Mytimer";
 function TodayDate({ isVisible }) {
   return (
     <>
-      {/* if isVible == true : affiche la clock */}
       <p>{isVisible && <MyClock/>}</p> 
-      {/* if isVible == false : affiche la timer */}
       <p>{!isVisible && <Mytimer/>}</p>
       <br />
     </>
